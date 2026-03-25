053694771732acf97f8d3402d2da3df4207457a3
Troy Gardner II <tgardner2@my.waketech.edu>
2026-03-25 08:08:25 -0400

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
+
 @pytest.fixture
 def freezing_point():
     """Returns the freezing point in all three units."""
     return {"C": 0.0, "F": 32.0, "K": 273.15}
+
 
 @pytest.fixture
 def boiling_point():
